4bf680e10fb26cde3835c728813718a4d0dbd21f
xiangdada <1365458967@qq.com>
2019-03-13 21:12:24 +0800

    插件功能初步测试

--- app/src/main/java/com/xdd/repluginproject/WelcomActivity.java
+++ app/src/main/java/com/xdd/repluginproject/WelcomActivity.java
@@ -1,13 +1,49 @@
 package com.xdd.repluginproject;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class WelcomActivity extends AppCompatActivity {
+import com.qihoo360.replugin.RePlugin;
+
+public class WelcomActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button toPlugTestMainActivityByPackageName;
+    private Button toPlugTestMainActivityByAlias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcom);
+        toPlugTestMainActivityByPackageName = findViewById(R.id.toPlugTestMainActivityByPackageName);
+        toPlugTestMainActivityByAlias = findViewById(R.id.toPlugTestMainActivityByAlias);
+        Toast.makeText(this, "" + BuildConfig.DEBUG, Toast.LENGTH_SHORT).show();
+        addListener();
+    }
+
+    private void addListener() {
+        toPlugTestMainActivityByPackageName.setOnClickListener(this);
+        toPlugTestMainActivityByAlias.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.toPlugTestMainActivityByPackageName:
+                RePlugin.startActivity(WelcomActivity.this,
+                        RePlugin.createIntent("com.example.xiangdada.test", "com.example.xiangdada.test.MainActivity"));
+                break;
+            case R.id.toPlugTestMainActivityByAlias:
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName("test", "com.example.xiangdada.test.MainActivity"));
+                RePlugin.startActivityForResult(WelcomActivity.this, intent, 100, null);
+                break;
+            default:
+                break;
+        }
     }
 }
