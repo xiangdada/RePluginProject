dba6281a80287cffa868aaed2011cc3dafc5693a
xiangdada <1365458967@qq.com>
2019-03-13 17:47:35 +0800

    Initial commit

---
+++ app/src/main/java/com/xdd/repluginproject/MyApplication.java
@@ -0,0 +1,13 @@
+package com.xdd.repluginproject;
+
+import android.app.Application;
+
+/**
+ * Created by xiangpengfei on 2019/3/13.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
